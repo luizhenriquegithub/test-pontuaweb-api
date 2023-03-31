af34ac7834b82e36ea1b28cfff1dffd363764e90
Luiz Henrique <luizhenriquegithub@gmail.com>
2023-03-31 16:33:36 -0300

    add first commit

---
+++ src/main/kotlin/com/pontuaweb/api/PontuawebApiApplication.kt
@@ -0,0 +1,11 @@
+package com.pontuaweb.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontuawebApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PontuawebApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/pontuaweb/api/PontuawebApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pontuaweb.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontuawebApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
